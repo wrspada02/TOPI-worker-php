abc981d7357202ec4f3064a34bbaa8b029254856
William Spada <90157791+wrspada02@users.noreply.github.com>
2026-05-27 00:42:06 +0000

    refactor: remove comments

--- app.php
+++ app.php
@@ -8,29 +8,18 @@ $queueName = 'items';
 $timeoutSeconds = 10;
 
 try {
-    // Create Predis client
     $redis = new Client([
         'scheme' => 'tcp',
         'host'   => '127.0.0.1',
         'port'   => 6379,
     ]);
 
-    // Test connection
     $redis->connect();
 
     echo "Connected to Redis using Predis\n";
     echo "Waiting for items on queue '{$queueName}'...\n";
 
     while (true) {
-
-        // BRPOP blocks until:
-        // - an item is available
-        // - or timeout occurs
-        //
-        // Returns:
-        // [queue_name, item]
-        // or null on timeout
-
         $result = $redis->brpop([$queueName], $timeoutSeconds);
 
         if ($result === null) {
@@ -41,9 +30,6 @@ try {
         [$queue, $item] = $result;
 
         echo "[" . date('Y-m-d H:i:s') . "] Popped item from '{$queue}': {$item}\n";
-
-        // Process item here
-        // ...
     }
 
 } catch (\Exception $e) {
